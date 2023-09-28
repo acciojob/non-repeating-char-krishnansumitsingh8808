847078178bf29dca2e216a5780c1ba58ab8e7d91
Sumit Raj Singh <133523998+krishnansumitsingh8808@users.noreply.github.com>
2023-09-28 07:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arrstring[]=...str;
+	for(let i in arrstring){
+		if(arrstring[i]!==arrstring[i+1]){
+			return arrstring[i];
+		}
+		else{
+			return null;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
