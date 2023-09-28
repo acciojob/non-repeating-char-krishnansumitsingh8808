e1b0da1db8801dcc2cecc2a451fb672b16524dfd
Sumit Raj Singh <133523998+krishnansumitsingh8808@users.noreply.github.com>
2023-09-28 08:14:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-	let arrstring=[...str];
-	for(let i in arrstring){
-		if(arrstring[i]!==arrstring[i+1]){
-			return arrstring[i];
-		}
-		else{
-			return null;
-		}
-	}
+  for (let i = 0; i < str.length; i++) {
+    let isRepeated = false;
+
+    for (let j = 0; j < str.length; j++) {
+      if (i !== j && str[i] === str[j]) {
+        isRepeated = true;
+        break; // Exit inner loop as soon as a repeat is found
+      }
+    }
+
+    if (!isRepeated) {
+      return str[i]; // Return the first non-repeated character
+    }
+  }
+
+  // If no character is non-repeated, return null
+  return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
